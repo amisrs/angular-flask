df0259406309f596c39cf4b449dc71d04e4aa5ec
amisrs <amisrs@amisrs.com>
2017-04-09 10:14:29 +0000

    woops

--- angular_flask/settings.py
+++ angular_flask/settings.py
@@ -2,4 +2,4 @@
 DEBUG = True
 SECRET_KEY = 'temporary_secret_key'  # make sure to change this
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:341adqwe@localhost:3306/project'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:dilgett@localhost:3306/project'
